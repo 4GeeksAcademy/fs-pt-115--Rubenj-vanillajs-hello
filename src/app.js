423a96df5c286374af63641288f35a27c14edcc1
RUB3N02 <rubyerizo@gmail.com>
2025-06-23 09:57:46 +0000

    Modificacion rpimera

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+pronoun.map((first) => {
+  adj.map((second) => {
+    noun.map((third) => {
+      const total = `${first}${second}${third}.com`
+      console.log(total)
+    })
+  })
+})
